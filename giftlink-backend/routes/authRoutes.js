fa1ff46a810a24a48682ea6bb137066246ac77d9
cupofguayusa <cupofguayusa@gmail.com>
2025-11-13 13:05:24 -0600

    Added login endpoint and logic

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -56,7 +56,68 @@ router.post("/register", async (req, res) => {
     res.status(201).json({ authtoken, email });
   } catch (e) {
     logger.error("Register error:", e.message);
-    return res.status(500).json({ error: "Internal server error", details: e.message });
+    return res
+      .status(500)
+      .json({ error: "Internal server error", details: e.message });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  console.log("\n\n Inside login");
+  try {
+    //Get email and password from body
+    const { email, password } = req.body;
+    logger.info("Starting Login Process...");
+
+    //Connect to Database
+    const db = await connectToDatabase();
+    logger.info("Connecting to Database...");
+
+    // Search for user
+    const collection = db.collection("users");
+    logger.info("Finding User...");
+    const theUser = await collection.findOne({ email: email });
+
+    // Does the user exist?
+    if (!theUser) {
+      logger.warn("Login failed: User not found");
+      return res.status(400).json({ error: "Invalid credentials" });
+    } else {
+      logger.info("User Found!");
+    }
+
+    //Check if password matches
+    if (theUser) {
+      //Grab user password
+      const userPassword = theUser.password;
+
+      let result = await bcryptjs.compare(password, userPassword);
+
+      if (!result) {
+        logger.error("Passwords do not match");
+        return res.status(400).json({ error: "Wrong password" });
+      }
+
+      let payload = {
+        user: {
+          id: theUser.insertedId.toString(),
+        },
+      };
+
+      //Fetch User Details
+      const userName = theUser.name;
+      const userEmail = theUser.email;
+
+      //Sign JWT Token
+      const authToken = jwt.sign(payload, JWT_SECRET);
+      logger.info("User logged in succesfully");
+      res.status(200).json({ authToken, userName, userEmail });
+    } else {
+      logger.error("User not found");
+      return res.status(404).json({ error: "User not found" });
+    }
+  } catch (e) {
+    return res.status(500).json("Internal Server Error");
   }
 });
 
